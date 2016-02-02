64e4c6e96ffdbd75180b8f46aeaccb1748ac1152
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-02-02 17:14:22 +0100

    Fix initial state
    It must come from redux-nabu.

--- src/index.js
+++ src/index.js
@@ -6,15 +6,14 @@ const StateDb      = require ('statedb').default;
 const transit      = require ('transit-immutable-js');
 const createStore  = require ('redux').createStore;
 const nabuReducer  = require ('redux-nabu').nabuReducer;
+const initState    = require ('redux-nabu').initialState;
 const addMessage   = require ('redux-nabu').addMessage;
 
 mkdirp (path.join (os.homedir (), '.epsitec'));
 const nabuDbPath   = path.join (os.homedir (), '.epsitec', 'nabu');
 const NabuDb       = new StateDb (nabuDbPath);
 const nabuState    = NabuDb.loadState ('nabu');
-const initialState = {
-  nabu: nabuState ? transit.fromJSON (nabuState) : {}
-};
+const initialState = nabuState ? transit.fromJSON (nabuState) : initState;
 
 const store        = createStore (nabuReducer, initialState);
 store.subscribe ( () => {
